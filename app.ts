f62ca9b5b8d662088d17b7c4837cd750f4eee4a8
MCiappina <mciappops@gmail.com>
2021-10-07 02:49:34 -0300

    working

--- app.ts
+++ app.ts
@@ -1,62 +1,97 @@
-const DemoFile = require("demofile");
+import { DemoFile, BaseEntity, CInferno, Player } from "demofile";
 const fs = require("fs");
 const chalk = require("chalk");
+var _ = require('lodash');
+
+//const SEED = 1146049601;
+const molotovExplode = 3580951569; // murmurHash2("inferno.start", SEED);
+const incExplode = 1110819271; // murmurHash2("inferno.start_incgrenade", SEED);
 
 fs.readFile("demo.dem", (err, buffer) => {
-  const demofile = new DemoFile.DemoFile();
+  const demofile = new DemoFile();
   const stats = {};
 
-  function log(grenade, userId) {
-    const player = demofile.players.find((player) => player.userId === userId);
-    const round = demofile.gameRules.roundsPlayed + 1;
+  function addGrenade(grenade, steamId) {
+    const player = demofile.players.find(
+      (player) => player.steamId === steamId
+    );
 
-    if (stats[player.userId] === undefined) {
-      stats[player.userId] = {
+    if (stats[player.steamId] === undefined) {
+      stats[player.steamId] = {
         name: player.name,
-        steamId: player.steamId,
-        [round]: {
-          Decoy: [],
-          Grenade: [],
-          Molotov: [],
-          Flashbang: [],
-        },
-        ...stats[player.userId],
+        utils: [],
       };
-      stats[player.userId][round][grenade].push(demofile.currentTick);
-    } else {
-      stats[player.userId] = {
-        ...stats[player.userId],
-        [round]: {
-          Decoy: [],
-          Grenade: [],
-          Molotov: [],
-          Flashbang: [],
-        },
-      };
-      stats[player.userId][round][grenade].push(demofile.currentTick);
     }
+
+    const grenadeStats = {
+      round: demofile.gameRules.roundsPlayed + 1,
+      tick: demofile.currentTick,
+      type: grenade,
+    };
+
+    stats[player.steamId].steamId = steamId
+    stats[player.steamId].utils.push(grenadeStats);
+  }
+
+  function logEndGame() {
+    const statsArray = Object.values(stats);
+    statsArray.forEach((player) => {
+      console.log(`${player["name"]} [${player["steamId"]}]`);
+      const rounds = Object.values(_.groupBy(player["utils"], "round"));
+      rounds.forEach(round => {
+        console.log(`Round ${round[0]["round"]}`)
+        const grenades = _.groupBy(round, "type");
+        for (let key in grenades){
+          console.log(`${key}: ${grenades[key].length}`)
+          grenades[key].forEach(grenade => {console.log(`-- ${grenade.tick}`)})
+        }
+      })
+      
+      console.log("----------")
+    })
   }
 
   demofile.gameEvents.on("decoy_detonate", (event) => {
-    log("Decoy", event.userid);
+    const thrower = demofile.entities.getByUserId(event.userid);
+    addGrenade("Decoy", thrower.steamId);
   });
 
   demofile.gameEvents.on("hegrenade_detonate", (event) => {
-    log("Grenade", event.userid);
+    const thrower = demofile.entities.getByUserId(event.userid);
+    addGrenade("Grenade", thrower.steamId);
   });
 
-  demofile.gameEvents.on("molotov_detonate", (event) => {
-    log("Molotov", event.userid);
+  demofile.on("svc_Sounds", (e) => {
+    for (const sound of e.sounds) {
+      if (
+        sound.soundNumHandle !== molotovExplode &&
+        sound.soundNumHandle !== incExplode
+      ) {
+        continue;
+      }
+
+      const inferno = (demofile.entities.entities.get(
+        sound.entityIndex
+      ) as unknown) as BaseEntity<CInferno>;
+
+      const thrower = inferno.owner as Player;
+      const isMolotov = sound.soundNumHandle === molotovExplode;
+
+      addGrenade(isMolotov ? "Molotov" : "Incendiary", thrower.steamId);
+    }
   });
 
   demofile.gameEvents.on("flashbang_detonate", (event) => {
-    log("Flashbang", event.userid);
+    const thrower = demofile.entities.getByUserId(event.userid);
+    addGrenade("Flashbang", thrower.steamId);
+  });
+  demofile.gameEvents.on("smokegrenade_detonate", (event) => {
+    const thrower = demofile.entities.getByUserId(event.userid);
+    addGrenade("Smoke", thrower.steamId);
   });
 
   demofile.on("end", () => {
-    Object.values(stats).forEach((entry) => {
-      console.log(entry);
-    });
+    logEndGame();
   });
 
   demofile.parse(buffer);
